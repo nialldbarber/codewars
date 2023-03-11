5196e918f6e049b84a46ec72c8a5427d35f02dec
Niall Barber <niall.barber@olioex.com>
2023-03-11 10:18:44 +0000

    Tests

--- rust/multiples-of-3-and-5/src/main.rs
+++ rust/multiples-of-3-and-5/src/main.rs
@@ -14,6 +14,36 @@
 	3 and 5, only count it once.
 */
 
+fn multiples_of_three_and_five(number: i32) -> i32 {
+    let mut multiples: Vec<i32> = Vec::new();
+    for i in 0..number {
+        if i % 3 == 0 {
+            multiples.push(i);
+        } else if i % 5 == 0 {
+            multiples.push(i)
+        }
+    }
+    let sum: i32 = multiples.iter().sum();
+    return sum;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = multiples_of_three_and_five(10);
+    println!("{}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiples_of_three_and_five_with_ten() {
+        let result = multiples_of_three_and_five(10);
+        assert_eq!(result, 23);
+    }
+    #[test]
+    fn test_multiples_of_three_and_five_with_larger_number() {
+        let result = multiples_of_three_and_five(100);
+        assert_eq!(result, 2318);
+    }
 }
